d1640e699c2164c827cc734f00c2943567777b21
Alicia <aliciaxd@live.se>
2023-12-20 18:32:46 +0100

    added comments to code

--- js/background.js
+++ js/background.js
@@ -1,10 +1,17 @@
+//API-key
 const key = "wxkOwFjjRJOPHzzcS9k3dNAqhwc1P15q0rEROTSoIXA";
+
+//API url
 const baseUrl =
   `https://api.unsplash.com/photos/random?client_id=${key}`;
 
+//Hämtar bodyn från htmlen för att kunna alägga bild som bakgrund
 const body = document.body;
+
+//Knapp för att generera ny bild
 const randomizeBackground = document.getElementById('randombackground')
 
+//Asynkron funktion som tar fram länk till bild
 async function getBackground(){
   try{
     let response = await fetch(baseUrl);
@@ -19,6 +26,8 @@ async function getBackground(){
   }
 }
 
+/*Asynkron funktion som tar länken från funktion ovan och kollar om 
+det finns sparad länk i localStorage, samt sätter bakgrundsbilden */
 async function setBackground(){
   let url = localStorage.getItem('background');
 
@@ -35,8 +44,10 @@ async function setBackground(){
   }
 }
 
+//Kallar på funktioner
 setBackground();
 
+//Knappen som genererar en ny bild med hjälp av att ta bort den sparade och generera en ny
 randomizeBackground.addEventListener('click', () => {
     localStorage.removeItem('background');
     setBackground();
